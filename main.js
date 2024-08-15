b198288cad485e052fd6bd276664b8d83c4c78f3
satvik1402 <153217866+satvik1402@users.noreply.github.com>
2024-08-16 00:37:56 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,31 @@
-/* ----- NAVIGATION BAR FUNCTION ----- */
-
-
-/* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
-    
-
-
-/* ----- TYPING EFFECT ----- */
-   
-
-/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
-   
-
-  /* -- HOME -- */
-  
-
-  /* -- PROJECT BOX -- */
-  
-
-  /* -- HEADINGS -- */
-  
-
-/* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
-
-  /* -- ABOUT INFO & CONTACT INFO -- */
-  
-
-  /* -- ABOUT SKILLS & FORM BOX -- */
-  
-
-
-/* ----- CHANGE ACTIVE LINK ----- */
-  
-  
+	// Sticky Navbar
+    let header = document.querySelector('header');
+    let menu = document.querySelector('#menu-icon');
+    let navbar = document.querySelector('.navbar');
+     
+     
+    window.addEventListener('scroll', () => {
+        header.classList.toggle('shadow', window.scrollY > 0);
+    });
+     
+    menu.onclick = () => {
+        navbar.classList.toggle('active');
+    }
+    window.onscroll = () => {
+        navbar.classList.remove('active');
+    }
+     
+    // Dark Mode
+    let darkmode = document.querySelector('#darkmode');
+     
+    darkmode.onclick = () => {
+        if(darkmode.classList.contains('bx-moon')){
+            darkmode.classList.replace('bx-moon','bx-sun');
+            document.body.classList.add('active');
+        }else{
+            darkmode.classList.replace('bx-sun','bx-moon');
+            document.body.classList.remove('active');
+        }
+    }
+ 
    
